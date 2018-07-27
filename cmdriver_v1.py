095ae9abbe9509c9a3e705ab847cae04bde3bf29
shalinisrma4 <41655362+shalinisrma4@users.noreply.github.com>
2018-07-27 11:23:50 -0400

    Add files via upload

--- cmdriver_v1.py
+++ cmdriver_v1.py
@@ -18,13 +18,15 @@ fout11 = open('C:\CMU\Courses\Python for Developers\stops_and_times.txt',
 stop_nos = list()
 
 for line in fin:
-        stop_nos.append(line.split(',')[1])
+        stop_nos.append(str(line.split(',')[1]))
 
 del stop_nos[0]
 
+print(stop_nos[0:2])
+
 # loop through all stops
 
-for st_no in stop_nos[0:3]:
+for st_no in stop_nos:
 
     # instantiate a chrome options object so you can set the size and headless preference
     chrome_options = Options()
@@ -42,7 +44,7 @@ for st_no in stop_nos[0:3]:
     #find stop number input box
     stpNumInput = driver.find_element_by_id("txtFindStopID")
     #enter stop number
-    stpNumInput.send_keys(str(st_no))
+    stpNumInput.send_keys((st_no))
 
     #click the find button
     findStop_button = driver.find_element_by_css_selector("[id=findStop]")
@@ -52,14 +54,16 @@ for st_no in stop_nos[0:3]:
     #get the first bus data
     busNumber = driver.find_element_by_id("route1").text
     eta = driver.find_element_by_id("time1").text
-    #print(st_no)
-    #print('bus#',busNumber)
-    #print('eta#',eta)
+    print(st_no)
+    print('bus#',busNumber)
+    print('eta#',eta)
 
     #save the bus data
     fout11.write(st_no + ", " + busNumber + ", " + eta +"\n")
 
 
-# capture the screen, name the file using current time
-#filename = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-#driver.get_screenshot_as_file(filename+'.png')
+    # capture the screen, name the file using current time
+    filename = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    driver.get_screenshot_as_file(filename+'.png')
+
+fout11.close()
