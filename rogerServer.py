1f21bb9e61fd21ccba7b552dee6331224d8dffd9
Vaibhav Walia <walia.vaibhav@yahoo.com>
2018-08-04 02:08:25 -0400

    Added bus table

--- rogerServer.py
+++ rogerServer.py
@@ -97,7 +97,7 @@ def scrape_bus_data(lat,lng):
         busNumber = (driver.find_element_by_id(route).text).strip()
         eta = (driver.find_element_by_id(eta).text).strip()
         if busNumber != '' and eta != '':
-            bus_data[busNumber] = eta
+            bus_data[i] = {'bus':busNumber, 'eta':eta}
 
     #return bus and stop Data
     data = {}
@@ -145,66 +145,6 @@ def getWeather(lat,lng):
     weather['recommendation'] = getRecommendation(weather['weather_code'])
     return weather
 
-# def weather_code_to_icon(code):
-#     map = {
-#     200 : '11d',
-#     201 : '11d',
-#     202 : '11d',
-#     210 : '11d',
-#     211 : '11d',
-#     212 : '11d',
-#     221 : '11d',
-#     230 : '11d',
-#     231 : '11d',
-#     232 : '11d',
-#     300	: '09d',
-#     301	: '09d',
-#     302	: '09d',
-#     310	: '09d',
-#     311	: '09d',
-#     312	: '09d',
-#     313	: '09d',
-#     314	: '09d',
-#     321 : '09d',
-#     500	: '10d',
-#     501	: '10d',
-#     502	: '10d',
-#     503	: '10d',
-#     504	: '10d',
-#     511	: '13d',
-#     520	: '09d',
-#     521	: '09d',
-#     522	: '09d',
-#     531	: '09d',
-#     600	: '13d',
-#     601	: '13d',
-#     602	: '13d',
-#     611	: '13d',
-#     612	: '13d',
-#     615	: '13d',
-#     616	: '13d',
-#     620	: '13d',
-#     621	: '13d',
-#     622	: '13d',
-#     701	: '50d',
-#     711	: '50d',
-#     721	: '50d',
-#     731	: '50d',
-#     741	: '50d',
-#     751	: '50d',
-#     761	: '50d',
-#     762	: '50d',
-#     771	: '50d',
-#     781	: '50d',
-#     800	: '01d', #can be '01n' at night
-#     801	: '02d',  #02n at night
-#     802	: '03d',  #03n at night
-#     803	: '04d',  #04n at night
-#     804	: '04d',  #04n at night
-#     }
-#     img_base_url = 'http://openweathermap.org/img/w/'
-#     return img_base_url + map.get(code) + '.png'
-
 '''Reads the stop info from stops.txt'''
 def getStopInfo():
     global stops
@@ -251,7 +191,7 @@ def getRecommendation(code):
     620	: 'Get an umbrella and layer up!',
     621	: 'Get an umbrella and layer up!',
     622	: 'Get an umbrella and layer up!',
-    701	: 'All set!',
+    701	: 'Get a light jacket',
     711	: 'Avoid travel',
     721	: 'All set!',
     731	: 'Dusty! Stay safe',
